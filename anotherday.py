fb2240b9a2098243745634db0bd15eb4cb48f036
Joao <hatsunemikutoro@gmail.com>
2025-10-30 10:47:55 -0300

    Separei por comentario

--- anotherday.py
+++ anotherday.py
@@ -3,25 +3,29 @@ import pygame
 
 pygame.init()
 
+# configurações
 w, h = 800,600
 window = pygame.display.set_mode((w,h))
 running = True
 clock = pygame.time.Clock()
 
+# player
 player_surf = pygame.Surface((30,30))
 player_surf.fill("black")
 player_rect = player_surf.get_frect(center=(w - 700, h / 2))
 
+# cano
 pipe_surf = pygame.Surface((100,500))
 pipe_surf.fill("green")
 pipe_rect = pipe_surf.get_frect(center=(100,100))
 
+# variaveis
 y_spd = 0
 jump_p = -350
 grv = 800
 spd = 200
 
-
+# loop principal
 while running:
     dt = clock.tick(60) / 1000
     for event in pygame.event.get():
